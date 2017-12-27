ebc3530f65ee9dac00df0b9256115d1fe73a3204
Sebastian Brückner <mail@invlid.com>
2017-12-27 15:10:01 +0100

    Add project skeleton

---
+++ Source/BoardingPassKit.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoardingPassKit.
+FOUNDATION_EXPORT double BoardingPassKitVersionNumber;
+
+//! Project version string for BoardingPassKit.
+FOUNDATION_EXPORT const unsigned char BoardingPassKitVersionString[];
